12013fe2fda6e27a98dd348ca8eeeae676a5d9b4
imaanee <mihoubi.imane1@gmail.com>
2023-11-15 22:10:04 +0100

    modify watchLater css

--- src/Pages/WatchLater/WatchLater.jsx
+++ src/Pages/WatchLater/WatchLater.jsx
@@ -2,7 +2,7 @@ import React, { Fragment, useContext } from "react";
 import { WatchListContext } from "../../providers/WatchList/WatchListState";
 import MovieCard from "../../components/MovieCard/MovieCard";
 import MovieTitle from "../../components/MovieDetails/MovieTitle/MovieTitle";
-import './WatchLater.css';
+import "./WatchLater.css"
 
 function WatchLater() {
   const { watchList } = useContext(WatchListContext);
@@ -10,18 +10,16 @@ function WatchLater() {
   return (
     <Fragment>
       <div className="movie-page">
+        <div className="header">
+          <h1 className="heading">My Watch List</h1>
+        </div>
         <div className="container">
-          <div className="header">
-            <h1 className="heading">My Watch List</h1>
-          </div>
-
           {watchList.length > 0 ? (
             <div className="movie-list" style={{ display: 'flex', flexWrap: 'wrap' }}>
               {watchList.map((movie) => (
                 <div className="movie-item" key={movie.id} style={{ marginRight: '20px' }}>
                   <MovieCard movie={movie} Images={process.env.REACT_APP_ImagesLink} />
                   <MovieTitle title={movie.title || movie.name} />
-                  {/**<MovieDescription>{movie.overview}</MovieDescription> **/ }
                 </div>
               ))}
             </div>
